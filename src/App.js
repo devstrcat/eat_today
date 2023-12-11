27f931f105422b745e77e605cf17b29f9134aeb7
soyeon <dev.vsoyeon@gmail.com>
2023-12-11 18:08:38 +0900

    feat : 상세 페이지 파일 생성

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Intro from "./pages/Intro";
 import Index from "./pages/Index";
 import { Wrap } from "./styles/basic";
 import { Route, Routes } from "react-router";
+import RecipeMore from "./pages/RecipeMore";
 
 const App = () => {
   return (
     <Wrap maxw={390} minw={320} minh={640}>
       <Routes>
         <Route path="/" element={<Intro />}></Route>
-        <Route path="/home" element={<Index />}></Route>
+        <Route path="/meal" element={<Index />}></Route>
+        <Route path="/meal/more" element={<RecipeMore />}></Route>
       </Routes>
     </Wrap>
   );
