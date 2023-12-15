2635400c86f14952e8fbb522601e78a0296202f3
soyeon <dev.vsoyeon@gmail.com>
2023-12-15 15:11:34 +0900

    feat : 상세페이지 emotion 최종 작업 완료

--- src/pages/RecipeMore.js
+++ src/pages/RecipeMore.js
@@ -1,35 +1,37 @@
 import React from 'react'
-import "../styles/recipemore.css";
 import Footer from '../components/Footer';
+import { BtWrap, CakeImg, ContentWrap, HashTagWrap, HeaderWrap, MoreMainWrap, MoreWrap, Title } from '../styles/more/moreStyle';
+import Search from '../components/Search';
 
 const RecipeMore = () => {
   return (
-    <div className='wrap'>
-        <header className='header'>
-            <div className='header-wrap'>
-            <div className='title'>초코케익</div>
-                <div className='bt-wrap'>
-                <button className='edit'>
-                </button>
-                <button className='trash'>   
-                </button>
-                </div>
-                </div>
-        </header>
-        <div className='main-wrap'>
-            <img src='/images/chocolate.svg' className='img'/>
-            <ul className='hashtag'>
-              <li>#달달구리</li>
+    <MoreWrap>
+      <Search></Search>
+            <HeaderWrap>
+            <Title>초코케이크</Title>
+                <BtWrap>
+                  <button className='edit'>
+                  </button>
+                  <button className='trash'>   
+                  </button>
+                </BtWrap>
+                </HeaderWrap>
+        <MoreMainWrap>
+            <CakeImg src='/images/chocolate.svg'/>
+            <HashTagWrap>
+              <li className='hashtag_1'>#달달구리</li>
               <li className='hashtag_2'>#초코케이크</li>
-              <li>#스트로베리초코</li>
+              <li className='hashtag_3'>#스트로베리초코</li>
               <li className='hashtag_4'>#베이킹원데이클래스</li>
-            </ul>
-            <div className='ingredient'></div>
-            <div className='recipe'></div>
-            <div className='comment'></div>
-        </div>
+            </HashTagWrap>
+            <ContentWrap>
+              <div className='ingredient'></div>
+              <div className='recipe'></div>
+              <div className='comment'></div>
+            </ContentWrap>
+        </MoreMainWrap>
         <Footer />
-    </div>
+    </MoreWrap>
   )
 }
 
