4203b78300ee025eb4899c5384da7de9e91ce129
songganam <dev.ganam@gmail.com>
2023-12-19 12:26:59 +0900

    feat : 작성페이지 데이터 연동

--- src/pages/RecipeWrite.js
+++ src/pages/RecipeWrite.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "../styles/recipewrite.css";
+import React, { useEffect, useState } from "react";
+
 import Footer from "../components/Footer";
 import {
   AddImages,
@@ -15,10 +15,23 @@ import {
   TextBoxes,
   WriteButtons,
 } from "../styles/recipewriteStyle";
+import { postWrite } from "../api/write/write_api";
+
+const initData = [];
 
 const RecipeWrite = () => {
+  // 목록 Post
+  const [writeData, setWriteData] = useState(initData);
+
+  // 한번 호출한다.
+  useEffect(() => {
+    postWrite(setWriteData);
+  });
+
   return (
-    <RecipeWriteWrap>
+    <div>
+      {writeData.map(item => (
+    <RecipeWriteWrap key={item.title}>
       <RecipeWriteTop>
         {/* 상단 취소 버튼 */}
         <CancelButton>
@@ -43,32 +56,39 @@ const RecipeWrite = () => {
         <TextBoxes>
           <input
             className="textboxes-title"
-            placeholder="제목을 입력해주세요."
+            placeholder={item.title}
           ></input>
           <input
             className="textboxes-ingre"
-            placeholder="재료를 입력해주세요."
+            placeholder={item.ingredient}
           ></input>
 
           {/* 해시 태그 */}
+
           <TextBoxHashTags>
             <input
-              className="textboxes-tags-today"
-              placeholder="#오늘의디저트"
+              type="text"
+              className="textboxes-tags-left"
+              placeholder={item.tags[0]}
             ></input>
+
             <input
-              className="textboxes-tags-chococake"
-              placeholder="#크리스마스초코케익"
+              type="text"
+              className="textboxes-tags-mid"
+              placeholder={item.tags[1]}
             ></input>
-            <div className="textboxes-tags-add">
-              <img src="/images/add_icon.svg"  />
+            <div className="textboxes-tags-right">
+              <img src={item.pics[4]} />
             </div>
           </TextBoxHashTags>
 
           {/* 레시피 기록 및 다이어리 코멘트 */}
           <RecipeComment>
-          <input className="textboxes-recipe" placeholder="Recipe" />
-          <input className="textboxes-comment" placeholder="Diary comment..." />
+            <input className="textboxes-recipe" placeholder={item.recipe} />
+            <input
+              className="textboxes-comment"
+              placeholder={item.review}
+            />
           </RecipeComment>
         </TextBoxes>
 
@@ -78,15 +98,99 @@ const RecipeWrite = () => {
             <img src="/images/reset_icon.png" />
           </div>
           <div className="complete-bt">
-            <img src="/images/check_icon.svg"  />
+            <img src="/images/check_icon.svg" />
           </div>
-  
         </WriteButtons>
-        
+
         <Footer></Footer>
       </RecipeWriteTop>
     </RecipeWriteWrap>
-  );
+
+      ))}
+    </div>
+   
+);
+
+
+
+
+
+  // return (
+  //   <RecipeWriteWrap>
+  //     <RecipeWriteTop>
+  //       {/* 상단 취소 버튼 */}
+  //       <CancelButton>
+  //         <CancelButtonIcon>
+  //           <img src="/images/cancel_icon.svg" />
+  //         </CancelButtonIcon>
+  //       </CancelButton>
+  //       {/* 이미지 추가 */}
+  //       <AddImages>
+  //         <AddImagesLeft>
+  //           <img src="/images/choco.png" />
+  //         </AddImagesLeft>
+  //         <AddImagesMid>
+  //           <img src="/images/add_icon.svg" />
+  //         </AddImagesMid>
+  //         <AddImagesRight>
+  //           <img src="/images/add_icon.svg" />
+  //         </AddImagesRight>
+  //       </AddImages>
+
+  //       {/* 텍스트 박스 */}
+  //       <TextBoxes>
+  //         <input
+  //           className="textboxes-title"
+  //           placeholder="제목을 입력해주세요."
+  //         ></input>
+  //         <input
+  //           className="textboxes-ingre"
+  //           placeholder="재료를 입력해주세요."
+  //         ></input>
+
+  //         {/* 해시 태그 */}
+
+  //         <TextBoxHashTags>
+  //           <input
+  //             type="text"
+  //             className="textboxes-tags-left"
+  //             placeholder="#해시태그를 넣어주세요."
+  //           ></input>
+
+  //           <input
+  //             type="text"
+  //             className="textboxes-tags-mid"
+  //             placeholder="#해시태그를 넣어주세요."
+  //           ></input>
+  //           <div className="textboxes-tags-right">
+  //             <img src="/images/add_icon.svg" />
+  //           </div>
+  //         </TextBoxHashTags>
+
+  //         {/* 레시피 기록 및 다이어리 코멘트 */}
+  //         <RecipeComment>
+  //           <input className="textboxes-recipe" placeholder="Recipe" />
+  //           <input
+  //             className="textboxes-comment"
+  //             placeholder="Diary comment..."
+  //           />
+  //         </RecipeComment>
+  //       </TextBoxes>
+
+  //       {/* 버튼 3가지 */}
+  //       <WriteButtons>
+  //         <div className="restart-bt">
+  //           <img src="/images/reset_icon.png" />
+  //         </div>
+  //         <div className="complete-bt">
+  //           <img src="/images/check_icon.svg" />
+  //         </div>
+  //       </WriteButtons>
+
+  //       <Footer></Footer>
+  //     </RecipeWriteTop>
+  //   </RecipeWriteWrap>
+  // );
 };
 
 export default RecipeWrite;
