cc5cae274c23b183bbb522daba54c7c745e0e6a9
BenedictBenjamin <siwoo98511@gmail.com>
2023-12-27 15:52:12 +0900

    feat: app.js 회원 가입 페이지 변수명 수정  SignUp :/meal/signup , LogIn :/meal/login

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -18,16 +18,16 @@ const Intro = () => {
   `;
 
   // 함수
-  // const navigate = useNavigate();
-  // useEffect(() => {
-  //   // 3초 후에 "/meal"으로 이동
-  //   const timeoutId = setTimeout(() => {
-  //     navigate("/meal");
-  //   }, 2000);
+  const navigate = useNavigate();
+  useEffect(() => {
+    // 3초 후에 "/meal"으로 이동
+    const timeoutId = setTimeout(() => {
+      navigate("/meal");
+    }, 2000);
 
-  //   // 컴포넌트가 언마운트될 때 타이머 제거
-  //   return () => clearTimeout(timeoutId);
-  // }, [navigate]);
+    // 컴포넌트가 언마운트될 때 타이머 제거
+    return () => clearTimeout(timeoutId);
+  }, [navigate]);
 
   return (
     <IntroDiv>
